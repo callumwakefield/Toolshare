41ef048f48c219b4352dd5c8a0da86b7f6522fc6
callumwakefield <78259957+callumwakefield@users.noreply.github.com>
2021-01-31 02:16:51 +1300

    Add files via upload

---
+++ V10 (Final iteration)/FAQ.php
@@ -0,0 +1,80 @@
+<?php
+include ('server.php');
+?>
+<html>
+<head>
+    <title>Frequently asked questions</title>
+    <meta charset="utf8"> 
+    <link rel="stylesheet" href="stylesheet.css">
+    <link rel="icon" href="images/icon.png"> 
+</head>
+<body>
+    
+    <div class="banner">
+        <div class="Toolshare">
+            Tool share    
+        </div>
+        <?php include('nav.php')?>
+    </div>    
+
+    <div class="parallex2">
+    <div class="parrlx2-title">
+            Frequently asked <br> questions
+        </div>
+    </div>
+    <div class="title-container">
+        
+    </div>
+    <div class="FAQ-content">
+        <p>
+            <br>
+        <strong>What is tool share?</strong>
+        <br>
+        Tool Share is a platform created to help people throughout 
+        New Zealand by allowing users to<br> upload information 
+        about their tools allowing others to contact them to borrow 
+        them<br> if needed. It also allows you to find tools that 
+        you need that are owned by other participants<br> allowing 
+        you to borrow them. This process is created to help users 
+        to save money by<br> preventing them from having to buy 
+        tools that may only be used once or twice and also<br> 
+        prevents the wastage that goes along with it.<br>
+        <br>
+        <br>
+
+        <strong>What can Toolshare do for you? </strong>
+        <br>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt<br> ut labore et dolore 
+        magna aliqua. Ut enim ad minim veniam, quis nostrud 
+        exercitation ullamco laboris<br> nisi ut aliquip ex ea 
+        commodo consequat. Duis aute<br> irure dolor in 
+        reprehenderit in voluptate velit esse cillum dolore eu 
+        fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+        non<br> proident, sunt in culpa qui officia deserunt mollit 
+        anim id est laborum.congue nisi vitae <br>suscipit tellus 
+        mauris a diam maecenas sed enim ut sem viverra aliquet eget 
+        sit amet tellus cras adipiscing enim<br> eu turpis egestas 
+        pretium aenean.<br>
+        <br>
+        <br>
+
+        <strong>How can I best utilise Toolshare? </strong>
+        <br>
+        Nisi porta lorem mollis aliquam ut porttitor leo a diam 
+        sollicitudin tempor id eu nisl nunc mi <br>ipsum faucibus 
+        vitae aliquet nec ullamcorper sit amet risus nullam eget 
+        felis eget nunc lobortis mattis <br>aliquam faucibus purus 
+        in massa tempor nec feugiat nisl pretium fusce id velit ut 
+        tortor pretium viverra suspendisse potenti <br>nullam ac 
+        tortor vitae purus faucibus ornare suspendisse sed nisi 
+        lacus sed viverra tellus in hac habitasse platea dictumst 
+        <br>vestibulum rhoncus est pellentesque.
+
+        </p>
+    </div>
+
+    <?php
+        include("footer.php");
+    ?>
+</body>
---
+++ V10 (Final iteration)/home.php
@@ -0,0 +1,48 @@
+<?php
+include ('server.php');
+?>
+<html>
+<head>
+    <title>About us</title>
+    <meta charset="utf8"> 
+    <link rel="stylesheet" href="stylesheet.css">
+    <link rel="icon" href="images/icon.png"> 
+</head>
+<body>
+    
+    <div class="banner">
+        <div class="Toolshare">
+            Tool share    
+        </div>
+        <?php include('nav.php')?>
+    </div>    
+
+    <div class="parallex3">
+        <div class="parrlx-title">
+            Home
+        </div>
+    </div>
+    
+    <div class="content">
+        <p>
+        <strong>Welcome to Toolshare</strong>
+        <br>
+        <br>
+        Tool Share is a platform created to help people throughout 
+        New Zealand by allowing users to<br> upload information 
+        about their tools allowing others to contact them to borrow 
+        them<br> if needed. It also allows you to find tools that 
+        you need that are owned by other participants<br> allowing 
+        you to borrow them. This process is created to help users 
+        to save money by<br> preventing them from having to buy 
+        tools that may only be used once or twice and also<br> 
+        prevents the wastage that goes along with it.<br>
+        <br>
+        </p>
+    </div>
+    <?php
+        include("footer.php");
+    ?>
+</body>
+
+
