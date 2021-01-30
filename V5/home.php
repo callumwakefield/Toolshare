99b891646f6a759c3296052ccd22540fc8f28d31
callumwakefield <78259957+callumwakefield@users.noreply.github.com>
2021-01-31 02:04:19 +1300

    Add files via upload

---
+++ V5/home.php
@@ -0,0 +1,22 @@
+<?php 
+    session_start();
+?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf8">
+        <link rel="stylesheet" href="style.css">
+        <link rel="icon" href="images/icon.png">
+        <title>Home</title>
+    </head>
+    <body>
+        <div id="nav_placeholder">
+            <?php 
+                include_once("nav.php");
+            ?>
+        </div>
+        <h1 class="test">Home</h1>
+    </body>
+</html>
+
+<script src="script.js"></script>
